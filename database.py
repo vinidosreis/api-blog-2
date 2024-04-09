f0558e56652183e13cd83ebcde6c527c675a0fb3
Charles Magno <magnocharles286@gmail.com>
2024-04-09 16:18:15 -0300

    BD concluido

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # URL de conexão com o banco de dados, por favor, insira dentro das aspas a URL de conexão com seu BD
-URL_DATABASE = ""
+URL_DATABASE = "mysql+pymysql://sql10697835:fiQMLXwTNi@sql10.freemysqlhosting.net/sql10697835"
 
 # Criação da engine
 engine = create_engine(URL_DATABASE)
