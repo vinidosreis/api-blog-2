805a9a7b1ca630405e7c012333b981455b7a9376
Charles Magno <magnocharles286@gmail.com>
2024-04-09 14:13:02 -0300

    Criado conexão com o banco de dados

---
+++ database.py
@@ -0,0 +1,14 @@
+# Essas são todas as importações necessárias para a conexão com Banco de Dados
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# URL de conexão com o banco de dados, por favor, insira dentro das aspas a URL de conexão com seu BD
+URL_DATABASE = ""
+
+# Criação da engine
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+base = declarative_base()
