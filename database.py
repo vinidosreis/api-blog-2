1e2e1d915df4b122ebb1a82efb9a1203d618ffa1
vinidosreis <vinidosreis13@gmail.com>
2024-04-12 03:40:43 -0300

    config heroku and database

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # URL de conexão com o banco de dados, por favor, insira dentro das aspas a URL de conexão com seu Bando de dados ! VLW
-URL_DATABASE = ""
+URL_DATABASE = "mysql+pymysql://sql10698258:uAHugtvUlh@sql10.freemysqlhosting.net:3306/sql10698258"
 
 # Criação da engine
 engine = create_engine(URL_DATABASE)
